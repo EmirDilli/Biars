f8a2441dd94afa597afbf3d0f6dff243f6492405
Ozan <can.gulbaz@ug.bikent.edu.tr>
2024-05-11 00:34:34 +0300

    sa

--- backend/src/routes/class.js
+++ backend/src/routes/class.js
@@ -7,6 +7,8 @@ const { classStatistic } = require("../controllers/class/classStatistic");
 const { questionAnalysis } = require("../controllers/class/questionAnalysis");
 const { report } = require("../controllers/class/report");
 const { getSemester } = require("../controllers/class/getSemesters");
+const { activeSemesters } = require("../controllers/class/activeSemesters");
+
 
 const router = Router();
 router.get("/:className/averages", isAuth, calculateAverages);
@@ -15,4 +17,5 @@ router.get("/:className/statistics", isAuth, classStatistic);
 router.get("/:className/question_analysis", isAuth, questionAnalysis);
 router.get("/:className/report", isAuth, report);
 router.get("/:className/semesters", isAuth, getSemester);
+router.get("/:className/activeSemesters", isAuth, activeSemesters);
 module.exports = router;
