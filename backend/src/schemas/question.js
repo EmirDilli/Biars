e55c51a80984968820c45f7d192cd184365ee73c
EmirDilli <emirkaan184@gmail.com>
2024-05-11 21:34:20 +0300

    Revert "merge trying"
    This reverts commit ba2c87b32542acf9d5f82b18fbb8bff48d11def9.

--- backend/src/schemas/question.js
+++ backend/src/schemas/question.js
@@ -1,15 +1,13 @@
+// Question Schema
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const questionSchema = new Schema({
-
-    url: {type: String, required: true},
-    solution: {type:String, required: false}, // Assuming file type is Buffer
-    class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required:false},
-    topics:{type: [String], required:false} 
-
-
+  url: String, // Assuming file type is Buffer
+  class: { type: mongoose.Schema.Types.ObjectId, ref: "Class" },
+  topics: [String],
 });
 
 const Question = mongoose.model("Question", questionSchema);
+
 module.exports = Question;
